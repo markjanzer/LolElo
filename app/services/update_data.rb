995d90c689d1d100128c8a7e0cea59d7a452a35b
markjanzer <mark.janzer@gmail.com>
2020-08-12 22:27:34 -0700

    Begin making script to update data

---
+++ app/services/update_data.rb
@@ -0,0 +1,25 @@
+class UpdateData
+  def initialize
+  end
+
+  def call
+    League.all.each do |league|
+      # Might need to deal with pagination here.
+      games_since_last_updated_at = PandaScore.request(
+        path: "matches", 
+        params: {
+          "filter[league_id]": league.id,
+          "range[end_at]": "#{last_updated_at.iso8601},#{DateTime.current.iso8601}"
+        }
+      )
+
+      games_since_last_updated_at.each do |new_game|
+        byebug
+      end
+    end
+  end
+
+  def last_updated_at
+    Match.maximum(:end_at)
+  end
+end
