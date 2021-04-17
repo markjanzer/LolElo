a803ca9c5c4f34a072b17810c24bb7840e5b9e1b
markjanzer <mark.janzer@gmail.com>
2021-04-17 12:07:53 -0700

    Run bundle update, start to create tests for league factory

--- spec/services/league_factory_spec.rb
+++ spec/services/league_factory_spec.rb
@@ -2,4 +2,16 @@
 
 RSpec.describe LeagueFactory do
   # Lets fill this out
+
+  describe "#call" do
+    subject { LeagueFactory.new(league_id: league_id, time_zone: time_zone).call }
+    let(:time_zone) { 'America/Los_Angeles' }
+
+    context "with an invalid league_id" do
+      allow(PandaScore).to receive(:league_data, league_id) { nil }
+      it "raises an error" do
+        expect(subject).to raise "Yo this ain't right"
+      end
+    end
+  end
 end
