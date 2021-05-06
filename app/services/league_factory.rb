ee79310cfcc607192201637dacd5dd09e0361442
markjanzer <mark.janzer@gmail.com>
2021-05-05 22:59:13 -0700

    Restrucutre/refactor LeagueFactory

--- app/services/league_factory.rb
+++ app/services/league_factory.rb
@@ -1,51 +1,48 @@
 # frozen_string_literal: true
 
 class LeagueFactory
-  def initialize(league_id:, time_zone:)
-    @league_id = league_id
+  def initialize(league_data:, time_zone:)
+    @league_data = league_data
     @time_zone = time_zone
   end
 
   def call
-    create_league
-    # create_series
-    league
+    if league_data.nil?
+      raise 'league_data is required'
+    end
+
+    if time_zone.nil?
+      raise 'time_zone is required'
+    end
+
+    initialize_league
   end
 
   private
 
-  attr_reader :league_id, :time_zone
+  attr_reader :league_data, :time_zone
 
-  def league_data
-    @league_data ||= PandaScore.league(league_id)
+  def initialize_league
+    League.find_or_initialize_by(
+      external_id: league_data['id'],
+      name: league_data['name'],
+      time_zone: time_zone
+    )
   end
 
-  def league
-    @league ||= League.find_or_initialize_by(external_id: league_id)
-  end
+  # def create_series
+  #   filtered_series_ids.each do |series_id|
+  #     SerieFactory.new(series_id).call
+  #   end
+  # end
 
-  def create_league
-    if league_data.nil?
-      raise "League not found"
-    end
+  # def filtered_series
+  #   league_data['series'].filter do |series|
+  #     series['full_name'].split.first.match?('Spring|Summer')
+  #   end
+  # end
 
-    league.name = league_data['name']
-    league.time_zone = time_zone
-  end
-
-  def create_series
-    filtered_series_ids.each do |series_id|
-      SerieFactory.new(series_id).call
-    end
-  end
-
-  def filtered_series
-    league_data['series'].filter do |series|
-      series['full_name'].split.first.match?('Spring|Summer')
-    end
-  end
-
-  def filtered_series_ids
-    filtered_series.pluck('id')
-  end
+  # def filtered_series_ids
+  #   filtered_series.pluck('id')
+  # end
 end
--- spec/services/league_factory_spec.rb
+++ spec/services/league_factory_spec.rb
@@ -1,40 +1,37 @@
 # frozen_string_literal: true
 
 RSpec.fdescribe LeagueFactory do
-  # Lets fill this out
-
   describe "#call" do
-    subject { LeagueFactory.new(league_id: league_id, time_zone: time_zone).call }
+    subject { LeagueFactory.new(league_data: league_data, time_zone: time_zone).call }
+    let(:league_data) { { "id" => 1, "name" => "First League" } }
     let(:time_zone) { 'America/Los_Angeles' }
 
-    context "with an invalid league_id" do
-      let(:league_id) { 0 }
+    context "without league_data" do
+      let(:league_data) { nil }
+
       it "raises an error" do
-        allow(PandaScore).to receive(:league_data).with(league_id).and_return(nil)
-        expect { subject }.to raise_error "League not found"
+        expect { subject }.to raise_error "league_data is required"
       end
     end
 
-    context "with a valid league_id" do
-      let(:league_id) { 1 }
+    context "without time_zone" do
+      let(:time_zone) { nil }
 
-      before do
-        allow(PandaScore).to receive(:league_data).with(league_id).and_return(
-          { "name" => "First League" }
-        )
+      it "raises an error" do
+        expect { subject }.to raise_error "time_zone is required"
       end
+    end
 
-      it "returns a league with set attributes" do
-        expect(subject).to have_attributes({
-          external_id: league_id,
-          name: "First League",
-          time_zone: time_zone
-        })
-      end
+    it "returns a league with set attributes" do
+      expect(subject).to have_attributes({
+        external_id: 1,
+        name: "First League",
+        time_zone: time_zone
+      })
+    end
 
-      it "does not create the league" do
-        expect(subject).to_not be_persisted
-      end
+    it "does not create the league" do
+      expect(subject).to_not be_persisted
     end
   end
 end
