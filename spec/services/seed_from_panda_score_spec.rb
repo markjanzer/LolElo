81e4aaac3da6cb6508af9fd7963667bf68aead02
markjanzer <mark.janzer@gmail.com>
2021-07-11 17:54:34 -0700

    Pass all basic tests for SeedFromPandaScore

--- spec/services/seed_from_panda_score_spec.rb
+++ spec/services/seed_from_panda_score_spec.rb
@@ -76,25 +76,64 @@ RSpec.describe SeedFromPandaScore do
       allow(PandaScore).to receive(:teams) { teams_data }
       allow(PandaScore).to receive(:matches) { matches_data }
       allow(PandaScore).to receive(:games) { games_data }
+
+      subject
     end
 
     context "when there is only one of each object" do
+
       it "creates one league" do
-        subject
         expect(League.count).to eq 1
       end
-      it "creates one serie"
-      it "creates a series that belongs to the league"
-      it "creates one tournament"
-      it "creates a tournament that belong to the serie"
-      it "creates two teams"
-      it "creates two teams that belong to the tournament"
-      it "creates one match"
-      it "creates a match that belongs to the tournament"
-      it "creates a match that has the two teams as opponents"
-      it "creates one game"
-      it "creates a game that belongs to the match"
-      it "creates a game that has one of the teams as a winner"
+
+      it "creates one serie" do
+        expect(Serie.count).to eq 1
+      end
+
+      it "creates a series that belongs to the league" do
+        expect(Serie.last.league).to eq League.last
+      end
+
+      it "creates one tournament" do
+        expect(Tournament.count).to eq 1
+      end
+
+      it "creates a tournament that belong to the serie" do
+        expect(Tournament.last.serie).to eq Serie.last
+      end
+
+      it "creates two teams" do
+        expect(Team.count).to eq 2
+      end
+
+      it "creates two teams that belong to the tournament" do
+        expect(Tournament.last.teams.pluck(:id)).to eq [Team.first.id, Team.second.id]
+      end
+
+      it "creates one match" do
+        expect(Match.count).to eq 1
+      end
+
+      it "creates a match that belongs to the tournament" do
+        expect(Match.first.tournament).to eq Tournament.first
+      end
+
+      it "creates a match that has the two teams as opponents" do
+        opponent_ids = [Match.first.opponent_1.id, Match.first.opponent_2.id]
+        expect(opponent_ids).to eq [Team.first.id, Team.second.id]
+      end
+
+      it "creates one game" do
+        expect(Game.count).to eq 1
+      end
+
+      it "creates a game that belongs to the match" do
+        expect(Game.first.match).to eq Match.first
+      end
+
+      it "creates a game that has one of the teams as a winner" do
+        expect(Game.first.winner).to eq Team.first
+      end
     end
 
   end
