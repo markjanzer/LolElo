e61bbbb49c26ea7ca2ac6d2d95db26bef901b036
markjanzer <mark.janzer@gmail.com>
2021-09-29 14:46:40 -0700

    Start rewriting tests+logic for league::create_snapshots

--- app/services/league/create_snapshots.rb
+++ app/services/league/create_snapshots.rb
@@ -9,19 +9,35 @@ class League
     def call
       raise "league not defined" unless league
 
+      # Get the games without snapshots
+      # Order them chronologically
+
+      # Destroy all snapshots that occur after the first of these games
+      # invalid_snapshots.destroy_all
+      # Create a snapshot for each game
+
       ordered_games.each do |game|
         Game::CreateSnapshots.new(game).call
       end
     end
 
-    # This doesn't belong here
-    def remove
-      Snapshot.destroy_all
-    end
-
     private
 
     attr_reader :league
+
+    def ordered_games_without_snapshots
+      game_ids = league.games.pluck(:id)
+      game_ids_with_snapshots = Snapshot.where(game_id: game_ids).pluck(:game_id)
+      Game.where(id: game_ids - game_ids_with_snapshots).order(end_at: :asc)
+    end
+
+    def earliest_snapshot_creation
+      ordered_games_without_snapshots.first.date
+    end
+
+    def invalid_snapshots
+      League.snapshots.where("date >= ?", earliest_snapshot_creation)
+    end
 
     def ordered_games
       league.games.order(end_at: :asc)
--- spec/services/league/create_snapshots_spec.rb
+++ spec/services/league/create_snapshots_spec.rb
@@ -15,6 +15,18 @@ RSpec.describe League::CreateSnapshots do
     let(:team1) { create(:team, name: "team1") }
     let(:team2) { create(:team, name: "team2") }
 
+=begin
+This spec file is testing some weird shit.
+It's stuff I wanted tested early, but I'm not really at risk of calculating elos
+out of order for two games in a match. Or at least I don't think so
+
+Here's what I want to test.
+For two separate games, it creates elos for each in the right order
+For series in separate years it creates reset elos for them
+It finds the first game without an elo and removes/recalculates elos after that point
+    From games as well as serie resets.
+=end
+
     context "if the league is not defined" do
       let(:league) { nil }
 
@@ -33,62 +45,79 @@ RSpec.describe League::CreateSnapshots do
       end
     end
 
-    context "when the league has one series" do
-      before { subject }
-
-      context "when the series has a match with two games" do
-        let(:s1_t1_matches) { create_list(:match, 1, opponent1: team1, opponent2: team2, games: s1_t1_m1_games) }
-        let(:s1_t1_m1_games) { [s1_t1_m1_game2, s1_t1_m1_game1] }
-        let(:s1_t1_m1_game1) { create(:game, winner: team1, end_at: "2020-01-02") }
-        let(:s1_t1_m1_game2) { create(:game, winner: team2, end_at: Date.parse("2020-01-02") + 1.hour) }
-        
-        it "calculates the games in the correct order" do
-          team1_elo, team2_elo = [1500, 1500]
-          team1_elo1, team2_elo1 = EloCalculator::GameResults.new(winner_elo: team1_elo, loser_elo: team2_elo).new_elos
-          team2_elo2, team1_elo2 = EloCalculator::GameResults.new(winner_elo: team2_elo1, loser_elo: team1_elo1).new_elos
-
-          expect(team1.elo).to eq team1_elo2
-          expect(team2.elo).to eq team2_elo2
-        end
-      end
-
-      context "when the series has two matches with a game each" do
-        let(:s1_t1_matches) { [s1_t1_match2, s1_t1_match1] }
-        let(:s1_t1_match1) { create(:match, opponent1: team1, opponent2: team2, games: [s1_t1_m1_game1], end_at: "2020-01-02") }
-        let(:s1_t1_m1_game1) { create(:game, winner: team1, end_at: "2020-01-02") }
-        let(:s1_t1_match2) { create(:match, opponent1: team1, opponent2: team2, games: [s1_t1_m2_game1], end_at: "2020-01-03") }
-        let(:s1_t1_m2_game1) { create(:game, winner: team2, end_at: "2020-01-03") }
-
-        it "calculates elos from the matches in the correct order" do
-          team1_elo, team2_elo = [1500, 1500]
-          team1_elo1, team2_elo1 = EloCalculator::GameResults.new(winner_elo: team1_elo, loser_elo: team2_elo).new_elos
-          team2_elo2, team1_elo2 = EloCalculator::GameResults.new(winner_elo: team2_elo1, loser_elo: team1_elo1).new_elos
-
-          expect(team1.elo).to eq team1_elo2
-          expect(team2.elo).to eq team2_elo2
-        end
-      end
-    end
-
-    context "when the league has two series in the same year" do
-      before { subject }
-
-      let(:series) { [serie2, serie1]}
+    
+    context "when there are two games without snapshots" do
       let(:s1_t1_matches) { create_list(:match, 1, opponent1: team1, opponent2: team2, games: s1_t1_m1_games) }
-      let(:s1_t1_m1_games) { create_list(:game, 1, winner: team1, end_at: "2020-01-02") }
+      let(:s1_t1_m1_games) { [s1_t1_m1_game2, s1_t1_m1_game1] }
+      let(:s1_t1_m1_game1) { create(:game, winner: team1, end_at: "2020-01-02") }
+      let(:s1_t1_m1_game2) { create(:game, winner: team2, end_at: Date.parse("2020-01-02") + 1.hour) }
       
-      let(:serie2) { create(:serie, tournaments: [s2_tournament], begin_at: "2020-06-01", year: 2020) }
-      let(:s2_tournament) { create(:tournament, teams: [team1, team2], matches: s2_t1_matches) }
-      let(:s2_t1_matches) { create_list(:match, 1, opponent1: team1, opponent2: team2, games: s2_t1_m1_games) }
-      let(:s2_t1_m1_games) { create_list(:game, 1, winner: team2, end_at: "2020-06-02") }
-
-      it "calculates the series in the correct order" do
+      it "calculates the games in the correct order" do
         team1_elo, team2_elo = [1500, 1500]
         team1_elo1, team2_elo1 = EloCalculator::GameResults.new(winner_elo: team1_elo, loser_elo: team2_elo).new_elos
         team2_elo2, team1_elo2 = EloCalculator::GameResults.new(winner_elo: team2_elo1, loser_elo: team1_elo1).new_elos
 
+        subject
+
         expect(team1.elo).to eq team1_elo2
         expect(team2.elo).to eq team2_elo2
+      end
+    end
+
+    xcontext "when the serie has three games, the first and third of which already has a elo" do
+      before { subject }
+
+      let(:s1_t1_matches) { create_list(:match, 1, opponent1: team1, opponent2: team2, games: s1_t1_m1_games) }
+      let(:s1_t1_m1_games) { [s1_t1_m1_game2, s1_t1_m1_game1, s1_t1_m1_game3] }
+      let(:s1_t1_m1_game1) { create(:game, winner: team1, end_at: "2020-01-02") }
+      let(:s1_t1_m1_game2) { create(:game, winner: team2, end_at: Date.parse("2020-01-02") + 1.hour) }
+      let(:s1_t1_m1_game3) { create(:game, winner: team2, end_at: Date.parse("2020-01-02") + 2.hours) }
+      let(:game1_team1_snapshot) { create(:snapshot, game: s1_t1_m1_game1, team: team1, elo: 1550, date: s1_t1_m1_game1.end_at) }
+      let(:game1_team2_snapshot) { create(:snapshot, game: s1_t1_m1_game1, team: team2, elo: 1450, date: s1_t1_m1_game1.end_at) }
+      let(:game3_team1_snapshot) { create(:snapshot, game: s1_t1_m1_game3, team: team1, elo: 1600, date: s1_t1_m1_game3.end_at) }
+      let(:game3_team2_snapshot) { create(:snapshot, game: s1_t1_m1_game3, team: team2, elo: 1400, date: s1_t1_m1_game3.end_at) }
+
+      it "creates snapshots for games without snapshots" do
+        expect(Snapshot.where(game: s1_t1_m1_game2).count).to eq 2
+      end
+
+      it "deletes the snapshots with a date that is after the games without snapshots" do
+        expect(game3_team1_snapshot).to_not be_present
+        expect(game3_team2_snapshot).to_not be_present
+      end
+
+      it "doesn't delete snapshots with a date that  is before games without snapshots" do
+        expect(game1_team1_snapshot).to be_present
+        expect(game1_team2_snapshot).to be_present
+      end
+    end
+
+    context "when the league has two series in different years" do
+      before { subject }
+
+      let(:series) { [serie0, serie1]}
+      let(:s1_t1_matches) { create_list(:match, 1, opponent1: team1, opponent2: team2, games: s1_t1_m1_games) }
+      let(:s1_t1_m1_games) { create_list(:game, 1, winner: team1, end_at: "2020-01-02") }
+      
+      let(:serie0) { create(:serie, tournaments: [s0_tournament], begin_at: "2019-06-01", year: 2020) }
+      let(:s0_tournament) { create(:tournament, teams: [team1, team2], matches: s0_t1_matches) }
+      let(:s0_t1_matches) { create_list(:match, 1, opponent1: team1, opponent2: team2, games: s0_t1_m1_games) }
+      let(:s0_t1_m1_games) { create_list(:game, 1, winner: team2, end_at: "2019-06-02") }
+
+      xit "generates a elo reset for both teams" do
+        # Include other columns when we add them
+        expect(Snapshot.where(team: team1, date: serie0.begin_at)).to be_present
+        expect(Snapshot.where(team: team2, date: serie0.begin_at)).to be_present
+      end
+
+      xcontext "when there are already reset snapshots for the second serie" do
+        let!(:t1_s1_reset_snapshot) { create(:snapshot, team: team1, elo: 1500, date: serie1.begin_at) }
+        let!(:t2_s1_reset_snapshot) { create(:snapshot, team: team2, elo: 1500, date: serie1.begin_at) }
+
+        it "removes the previous elo resets" do
+          expect(t1_s1_reset_snapshot).not_to be_present
+          expect(t2_s1_reset_snapshot).not_to be_present
+        end
       end
     end
   end
