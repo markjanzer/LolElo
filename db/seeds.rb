4bc917dee7ca779575c429b18da14c44d84fa579
Mark Janzer <mark.janzer@gmail.com>
2023-11-03 16:24:24 -0700

    Reformat seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,14 @@
 # frozen_string_literal: true
 
-# This will need to be ran multiple times
-result = NewSeeder::SeedFromPandaScore.new.call
+# This populates from the API into PandaScore objects
+# This will take several hours with sidekiq, ideally is only run once.
+# ThirdSeeder::Seed.call
 
-if result == "Done!"
-  League.all.each do |league|
-    League::CreateSnapshots.new(league).call
-  end
+# This populates application objects from the PandaScore objects
+PandaScore::League.all.each do |league|
+end
+
+# This creates snapshots from the application objects
+League.all.each do |league|
+  League::CreateSnapshots.new(league).call
 end
