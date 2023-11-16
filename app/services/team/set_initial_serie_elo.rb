fcd9ec6f0201564c290de0af664b15720c5ce03e
Mark Janzer <mark.janzer@gmail.com>
2023-11-16 15:17:18 -0800

    Revamp code for setting initial elo

--- app/services/team/set_initial_serie_elo.rb
+++ app/services/team/set_initial_serie_elo.rb
@@ -7,32 +7,13 @@ class Team
 
     def call
       # If there is a snapshot for this team in the serie's season, do nothing
-      # If there is a snapshot for this team in the previous season, reset the elo
+      return if team.snapshots.where(serie: same_year_series).present?
+
+      # If there is a snapshot for this team in the previous season, revert the elo
+      return create_reverted_elo if team.snapshots.where(serie: previous_year_series).present?
+
       # If there is no snapshot for this team in either, then set a new elo
-
-
-
-      # If the team has an elo from a previous serie this year, do nothing
-      # league = serie.league
-      # series_in_this_year = league.series.where(year: serie.year)
-      # puts series_in_this_year
-      # puts team.snapshots.where(serie: series_in_this_year)
-      # return if team.snapshots.where(serie: series_in_this_year).present?
-      
-      # This doesn't create a snapshot if any snapshot has been created since the series
-      # begin_at. I don't like this because it enforces a chronological order of operations
-      # Oh I guess that chronological is necessary to some extent though.
-      return if team.snapshots.where("datetime >= ?", serie.begin_at).present?
-
-      # This is not the logic I want
-      # For instance, if a team did not participate in the 2021 Championship serie,
-      # then they would get a new_team elo at the beginning of of 2022 because they
-      # were not in the previous serie.
-      if !previous_serie || !team_in_previous_serie
-        create_new_team_elo
-      elsif previous_serie_in_other_season
-        create_reverted_elo
-      end 
+      create_new_team_elo
     end
 
     private
@@ -41,19 +22,12 @@ class Team
 
     delegate :league, to: :serie
 
-    def previous_serie
-      @previous_serie ||= league.series
-        .where("begin_at < ?", serie.begin_at)
-        .order(begin_at: :desc)
-        .first
+    def same_year_series
+      league.series.where(year: serie.year)
     end
 
-    def team_in_previous_serie
-      previous_serie && previous_serie.teams.include?(team)
-    end
-
-    def previous_serie_in_other_season
-      previous_serie && previous_serie.year != serie.year
+    def previous_year_series
+      league.series.where(year: serie.year - 1)
     end
 
     def create_new_team_elo
--- spec/services/team/set_initial_serie_elo_spec.rb
+++ spec/services/team/set_initial_serie_elo_spec.rb
@@ -31,10 +31,11 @@ RSpec.describe Team::SetInitialSerieElo do
 
     context "when the team has an elo from another serie in the same year" do
       it "does nothing" do
-        setup => { team:, serie: }
+        setup => { team:, serie:, league: }
 
         other_serie = create(:serie, league: league, begin_at: "2020-01-01", year: 2020)
-        # Not sure if this works
+        other_tournament = create(:tournament, serie: other_serie)
+        create(:teams_tournament, team: team, tournament: other_tournament)
         create(:snapshot, team: team, serie: other_serie)
 
         expect { described_class.new(team: team, serie: serie).call }.not_to change { Snapshot.count }
@@ -43,13 +44,15 @@ RSpec.describe Team::SetInitialSerieElo do
 
     context "when the team no elo from the current year" do
       context "when the team has elo from the previous year" do
-        it "creates a snapshot with reset elo" do
-          setup => { team:, serie: }
+        it "creates a snapshot with reverted elo" do
+          setup => { team:, serie:, league: }
 
-          old_serie = create(:serie, league: league, begin_at: "2019-01-01", year: 2019)
-          old_snapshot = create(:snapshot, elo: 1200, team: team, serie: old_serie)
+          last_year_serie = create(:serie, league: league, begin_at: "2019-01-01", year: 2019)
+          last_year_tournament = create(:tournament, serie: last_year_serie)
+          create(:teams_tournament, team: team, tournament: last_year_tournament)
+          last_year_snapshot = create(:snapshot, elo: 1200, team: team, serie: last_year_serie)
 
-          reverted_elo = EloCalculator::Revert.new(old_snapshot.elo).call
+          reverted_elo = EloCalculator::Revert.new(last_year_snapshot.elo).call
 
           expect { described_class.new(team: team, serie: serie).call }.to change { Snapshot.count }.by 1
 
@@ -58,9 +61,11 @@ RSpec.describe Team::SetInitialSerieElo do
         end
 
         it "creates a snapshot with correct attributes" do
-          setup => { team:, serie: }
+          setup => { team:, serie:, league: }
 
           old_serie = create(:serie, league: league, begin_at: "2019-01-01", year: 2019)
+          old_tournament = create(:tournament, serie: old_serie)
+          create(:teams_tournament, team: team, tournament: old_tournament)
           old_snapshot = create(:snapshot, elo: 1200, team: team, serie: old_serie)
 
           reverted_elo = EloCalculator::Revert.new(old_snapshot.elo).call
@@ -75,6 +80,26 @@ RSpec.describe Team::SetInitialSerieElo do
             datetime: serie.begin_at,
             elo: reverted_elo
           )          
+        end
+
+        context "there was an earlier serie in the current year" do
+          it "creates a snapshot with reverted elo" do
+            setup => { team:, serie:, league: }
+
+            _pervious_serie_from_same_year = create(:serie, league: league, begin_at: "2020-01-01", year: 2020)
+
+            last_year_serie = create(:serie, league: league, begin_at: "2019-01-01", year: 2019)
+            last_year_tournament = create(:tournament, serie: last_year_serie)
+            create(:teams_tournament, team: team, tournament: last_year_tournament)
+            last_year_snapshot = create(:snapshot, elo: 1200, team: team, serie: last_year_serie)
+
+            reverted_elo = EloCalculator::Revert.new(last_year_snapshot.elo).call
+
+            expect { described_class.new(team: team, serie: serie).call }.to change { Snapshot.count }.by 1
+
+            snapshot = Snapshot.last
+            expect(snapshot.elo).to eq(reverted_elo)
+          end
         end
       end
 
@@ -101,6 +126,19 @@ RSpec.describe Team::SetInitialSerieElo do
             datetime: serie.begin_at,
             elo: EloCalculator::NEW_TEAM_ELO
           )
+        end
+
+        context "there was an earlier serie in the current year" do
+          it "creates a snapshot with new_team elo" do
+            setup => { team:, serie:, league: }
+
+            _pervious_serie_from_same_year = create(:serie, league: league, begin_at: "2020-01-01", year: 2020)
+
+            expect { described_class.new(team: team, serie: serie).call }.to change { Snapshot.count }.by 1
+
+            snapshot = Snapshot.last
+            expect(snapshot.elo).to eq(EloCalculator::NEW_TEAM_ELO)
+          end
         end
       end
     end
