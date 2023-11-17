8c9d209e185364ebf76d45efa17489ea20399e42
Mark Janzer <mark.janzer@gmail.com>
2023-11-16 19:19:17 -0800

    Fix issue with games starting before series

--- app/services/application_seeder/create_or_update_game.rb
+++ app/services/application_seeder/create_or_update_game.rb
@@ -1,5 +1,7 @@
 module ApplicationSeeder
   class CreateOrUpdateGame
+    include Memery
+    
     def initialize(panda_score_game)
       @panda_score_game = panda_score_game
     end
@@ -7,12 +9,19 @@ module ApplicationSeeder
     def call
       return if forfeit?
 
-      game = Game.find_or_initialize_by(panda_score_id: panda_score_game.panda_score_id)
-      game.update!(
-        end_at: end_at,
-        winner: panda_score_game.winner,
-        match: panda_score_game.match
-      )
+      Game.transaction do
+        game = Game.find_or_initialize_by(panda_score_id: panda_score_game.panda_score_id)
+
+        if serie_begins_after_game?
+          set_serie_begin_at_before_game_end_at
+        end
+
+        game.update!(
+          end_at: end_at,
+          winner: panda_score_game.winner,
+          match: panda_score_game.match
+        )
+      end
     end
 
     private
@@ -24,13 +33,25 @@ module ApplicationSeeder
     end
 
     # There is at least one game (ps_id: 149787) without an end_at that wasn't forfeited.
-    def end_at
+    memoize def end_at
       return panda_score_game.data["end_at"] unless panda_score_game.data["end_at"].nil?
 
       # Not sure if I want this. Maybe I want things to break?
       return nil if panda_score_game.data["begin_at"].nil? || panda_score_game.data["length"].nil?
 
       DateTime.parse(panda_score_game.data["begin_at"]) + panda_score_game.data["length"].seconds
+    end
+
+    memoize def serie
+      panda_score_game.match.tournament.serie
+    end
+
+    def serie_begins_after_game?
+      serie.begin_at && end_at < serie.begin_at
+    end
+
+    def set_serie_begin_at_before_game_end_at
+      serie.update!(begin_at: DateTime.parse(end_at) - 1.minute)
     end
   end
 end
--- spec/services/application_seeder/create_or_update_game_spec.rb
+++ spec/services/application_seeder/create_or_update_game_spec.rb
@@ -71,6 +71,28 @@ RSpec.describe ApplicationSeeder::CreateOrUpdateGame do
       end
     end
 
+    context "when the end date is less than the serie's start_at" do
+      it "updates the series start_at to be one minute before the game's end_at" do
+        game_end_at =  DateTime.parse("2020-01-05 00:00:00 UTC")
+        serie_begin_at = game_end_at + 3.minutes
+        
+        panda_score_game = create(:panda_score_game, data: {
+          "end_at"=> game_end_at,
+        })
+        serie = create(:serie, begin_at: serie_begin_at)
+        tournament = create(:tournament, serie: serie)
+        match = create(:match, tournament: tournament)
+        allow(panda_score_game).to receive(:match).and_return(match)
+
+        team1 = create(:team)
+        allow(panda_score_game).to receive(:winner).and_return(team1)
+
+        described_class.new(panda_score_game).call
+
+        expect(serie.reload.begin_at).to eq(game_end_at - 1.minute)
+      end
+    end
+
     it "sets the game with correct attributes" do
       match = create(:match)
       end_at = DateTime.parse("2020-01-01")
@@ -79,7 +101,6 @@ RSpec.describe ApplicationSeeder::CreateOrUpdateGame do
         "match_id" => match.panda_score_id,
         "end_at"=> end_at
       })
-
 
       team1 = create(:team)
       allow(panda_score_game).to receive(:winner).and_return(team1)
