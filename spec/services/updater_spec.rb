beee2c11387e52e0f8bf7d83ec96187a7f1d820e
Mark Janzer <mark.janzer@gmail.com>
2024-06-05 16:45:15 -0700

    Start writing general tests for updater_spec

--- spec/services/updater_spec.rb
+++ spec/services/updater_spec.rb
@@ -4,10 +4,31 @@ require 'rails_helper'
 
 RSpec.describe Updater do
   describe "#call" do
-    it "calls create_new_series with each league"
+    it "calls create_new_series with each league" do
+      league = create(:league)
+      instance = described_class.new
+      expect(instance).to receive(:create_new_series).with(league)
+      instance.call
+    end
+
+    # This is a little tricky because it continues to run the code
+    # Which attempts to access the API, and I don't want to spec everything out
+    # This might be a little easier when I move logic around.
     it "calls create_new_tournaments with each ps_serie that hasn't finished"
+
     it "updates the ps_serie"
     it "does not do anything if the serie is complete"
+    
+    it "calls create_new_matches with each non-complete tournament"
+    it "update each non-complete tournament"
+    it "does nothing if the tournament is complete"
+
+    it "calls create_new_games for each non-complete match"
+    it "updates each non-complete match"
+    it "does nothing if the match is complete"
+
+    it "updates each non-complete game"
+    it "does nothing if the game is complete"
   end
 
   describe "#create_new_series" do
