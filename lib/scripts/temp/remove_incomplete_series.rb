10909ea975e5d6bcfa6de2f7db8b0a270d02ddc0
Mark Janzer <mark.janzer@gmail.com>
2024-08-14 13:08:19 -0700

    Change script to reset elo as well as remove the series

--- lib/scripts/temp/remove_incomplete_series.rb
+++ lib/scripts/temp/remove_incomplete_series.rb
@@ -2,3 +2,9 @@
 INCOMPLETE_SERIE_IDS = [39, 426]
 
 Serie.where(panda_score_id: INCOMPLETE_SERIE_IDS).destroy_all
+
+league = League.find_by(name: "LPL")
+# 2017 Spring
+first_serie = league.series.find_by(full_name: "Spring 2017")
+# Recreate snapshots from beginning of 2017 Spring onwards
+EloSnapshots::LeagueProcessor.call(league, first_serie.begin_at)
