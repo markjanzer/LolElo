cb04f0df783faa2cbc76f1ceafdadb966ffd96b3
Mark Janzer <mark.janzer@gmail.com>
2024-08-20 13:32:45 -0700

    Add redis

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,10 +1,17 @@
-schedule_file = "config/schedule.yml"
+redis_url = ENV['REDISCLOUD_URL'] || 'redis://localhost:6379/0'
 
+Sidekiq.configure_server do |config|
+  config.redis = { url: redis_url }
+end
+
+schedule_file = "config/schedule.yml"
 if File.exist?(schedule_file) && Sidekiq.server?
   Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
 end
 
-# Mute sidekiq logs in tests
 Sidekiq.configure_client do |config|
+  # Mute sidekiq logs in tests
   config.logger = Rails.env.test? ? Logger.new(nil) : Sidekiq.logger
+
+  config.redis = { url: redis_url }
 end
