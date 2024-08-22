4d416d8381ab8b35a44a679f1fea1bf1e8603771
Mark Janzer <mark.janzer@gmail.com>
2024-08-22 12:15:02 -0700

    Move a lot of styles into tailwind from module.css

--- app/javascript/components/Chart.jsx
+++ app/javascript/components/Chart.jsx
@@ -24,9 +24,9 @@ export const Chart = ({ data }) => {
 
   function customToolTip(props) {
     return (
-      <div className={styles.toolTip}>
-        <div className={styles.toolTipTitle}>{props.label}</div>
-        <ul className={styles.matches}>
+      <div>
+        <div className="text-2xl text-center">{props.label}</div>
+        <ul className="my-2 mx-4">
           {matchData
             .filter((d) => d.date == props.label)
             .map((datum) => {
@@ -38,45 +38,40 @@ export const Chart = ({ data }) => {
   }
 
   function renderEloChange(changeAmount) {
-    let changeStyle, formattedAmount;
     if (changeAmount < 0) {
-      changeStyle = styles.negativeChange;
-      formattedAmount = changeAmount.toString();
+      return <div className={"text-red-600"}>{changeAmount}</div>;
     } else {
-      changeStyle = styles.positiveChange;
-      formattedAmount = "+ " + changeAmount.toString();
+      return <div className={"text-green-600"}>{`+ ${changeAmount}`}</div>;
     }
-
-    return <div className={changeStyle}>{formattedAmount}</div>;
   }
 
   function renderMatch(match) {
     let score = `${match.opponent1_score} - ${match.opponent2_score}`;
 
     return (
-      <li className={styles.match}>
-        <div className={styles.opponent}>
+      <li className="flex m-2 bg-purple-popout rounded border border-green-accent">
+        <div className="mx-3 my-2 flex flex-col items-center">
           <div
-            className={styles.opponentAcronym}
+            className="text-2xl"
             style={{ borderBottom: `2px solid ${match.opponent1.color}` }}
           >
             {match.opponent1.acronym}
           </div>
-          <div className={styles.opponentEloData}>
-            <div className={styles.opponentElo}>{match.opponent1_elo}</div>
+          <div className="flex flex-col items-end">
+            <div>{match.opponent1_elo}</div>
             {renderEloChange(match.opponent1_elo_change)}
           </div>
         </div>
-        <div className={styles.matchResult}>{score}</div>
-        <div className={styles.opponent}>
+        <div>{score}</div>
+        <div className="mx-3 my-2 flex flex-col items-center">
           <div
-            className={styles.opponentAcronym}
+            className="text-2xl"
             style={{ borderBottom: `2px solid ${match.opponent2.color}` }}
           >
             {match.opponent2.acronym}
           </div>
-          <div className={styles.opponentEloData}>
-            <div className={styles.opponentElo}>{match.opponent2_elo}</div>
+          <div className="flex flex-col items-end">
+            <div>{match.opponent2_elo}</div>
             {renderEloChange(match.opponent2_elo_change)}
           </div>
         </div>
@@ -135,7 +130,7 @@ export const Chart = ({ data }) => {
     const dateData = lineChartData.filter((d) => d.name === selectedDate)[0];
     const formattedDateData = teamData.reduce((result, team) => {
       const datum = {
-        acronym: team.acronym,
+        name: team.name,
         elo: dateData[team.acronym],
         color: team.color,
       };
@@ -147,16 +142,16 @@ export const Chart = ({ data }) => {
     );
 
     return (
-      <div class={styles.teamElosList}>
-        <h2 class={styles.selectedDate}>{selectedDate}</h2>
+      <div className="my-1">
+        <h2 className="text-2xl mx-2 my-4 text-green-accent">{selectedDate}</h2>
         <ul>
           {sortedFormattedDateData.map((datum) => {
             return (
               <li 
                 key={datum.acronym}
-                className={styles.teamListItem}
+                className="m-2"
               >
-                {datum.acronym}: {datum.elo}
+                {datum.name}: {datum.elo}
               </li>
             );
           })}
--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   content: [
     './public/*.html',
     './app/helpers/**/*.rb',
-    './app/javascript/**/*.js',
+    './app/javascript/**/*.{js,jsx}',
     './app/views/**/*.{erb,haml,html,slim}'
   ],
   theme: {
