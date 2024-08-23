f22c5b2698742f59791f5929c6635bd947fdb49e
Mark Janzer <mark.janzer@gmail.com>
2024-08-22 18:50:28 -0700

    Ensure space is still taken even when a league has no series in a year

--- app/controllers/series_controller.rb
+++ app/controllers/series_controller.rb
@@ -2,23 +2,20 @@
 
 class SeriesController < ApplicationController
   def index
-    @series_by_year_and_league = Serie
-      .includes(:league)
-      .order(year: :desc, begin_at: :desc)
-      .group_by(&:year)
-      .transform_values do |series|
-        series.group_by(&:league)
-              .transform_keys(&:name)
-              .transform_values do |league_series|
-                league_series.map do |serie|
-                  {
-                    name: serie.full_name,
-                    url: "series/#{serie.id}"
-                  }
-                end
-              end
-              .sort_by { |league_name, _| League::LEAGUE_ORDER[league_name] }
+    years = Serie.distinct.order(year: :desc).pluck(:year)
+    leagues = League.all.sort_by { |league| League::LEAGUE_ORDER[league.name] }
+
+    series_data = Serie.includes(:league)
+      .where(year: years)
+      .order(begin_at: :desc)
+      .group_by { |serie| [serie.year, serie.league_id]}
+
+    @series_by_year_and_league = years.each_with_object({}) do |year, result|
+      result[year] = leagues.each_with_object({}) do |league, league_data|
+        league_series = series_data[[year, league.id]] || []
+        league_data[league.name] = league_series.map { |serie| { name: serie.full_name, url: "series/#{serie.id}" } }
       end
+    end
   end
   
   def show
@@ -30,7 +27,5 @@ class SeriesController < ApplicationController
       .order(begin_at: :asc)
       .first
     @chart_data = ChartData.new(@serie).call
-
-    # Serie.all.map { |s| { id: s.id, fnl: s.full_name.length }.sort_by { |s| s[:fnl] } }
   end
 end
