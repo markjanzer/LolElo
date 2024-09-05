284c412056cc79119d67397d4f301346d17245df
Mark Janzer <mark.janzer@gmail.com>
2024-09-04 17:41:37 -0700

    Remove sql file, add metadata for what_is_elo page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,6 @@
 class StaticPagesController < ApplicationController
   def what_is_elo
+    @page_title = "What is Elo?"
+    @page_description = "Learn about the Elo rating system and how it is used to rank and compare the skill levels of players and teams in competitive games like League of Legends."
   end
 end
